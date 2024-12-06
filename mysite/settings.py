cd0ea322b02153c3331d91e9d1e6f5c9d036f2e9
EnnaLea <soleariaeve@gmail.com>
2024-12-06 10:11:52 +0100

    changing mail

--- mysite/settings.py
+++ mysite/settings.py
@@ -129,5 +129,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'soleariaeve@gmail.com'
+EMAIL_HOST_USER = 'dekulandmidoriya@gmail.com'
 EMAIL_HOST_PASSWORD= 'lkkr rzfh vsui jrmc '
